965c8c61bd3182f048a57c25bfb096b7bd137cb3
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2020-05-04 20:42:49 +0200

    service and resolver for scripture; #2_GraphQL_Enrichment

--- src/scripture/scripture.resolver.ts
+++ src/scripture/scripture.resolver.ts
@@ -1,4 +1,32 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { ScriptureService } from './scripture.service';
+import { ScriptureType } from './dto/create-scripture.dto';
+import { ScriptureInput } from './model/input-scripture.model';
 
-@Resolver('Scripture')
-export class ScriptureResolver {}
+@Resolver()
+export class ScriptureResolver {
+  constructor(private readonly itemsService: ScriptureService) {}
+
+  @Query(() => [ScriptureType])
+  async items(): Promise<ScriptureType[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Mutation(() => ScriptureType)
+  async createScripture(@Args('input') input: ScriptureInput): Promise<ScriptureInput> {
+    return this.itemsService.create(input);
+  }
+
+  @Mutation(() => ScriptureType)
+  async updateScripture(
+    @Args('id') id: string,
+    @Args('input') input: ScriptureInput,
+  ): Promise<ScriptureInput> {
+    return this.itemsService.update(id, input);
+  }
+
+  @Mutation(() => ScriptureType)
+  async deleteScripture(@Args('id') id: string): Promise<ScriptureInput> {
+    return this.itemsService.delete(id);
+  }
+}
